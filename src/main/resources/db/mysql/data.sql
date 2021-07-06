f4b92802b69d28c868813e7e50f022f183aa88bf
Will Doyle <41004538+willjdoyle@users.noreply.github.com>
2021-07-06 11:43:12 -0400

    Turtle Time
    I want to be able to add Yertle the turtle.

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -24,6 +24,7 @@ INSERT IGNORE INTO types VALUES (6, 'hamster');
 INSERT IGNORE INTO types VALUES (7, 'turtle');
 
 
+
 INSERT IGNORE INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023');
 INSERT IGNORE INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749');
 INSERT IGNORE INTO owners VALUES (3, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFarland', '6085558763');
